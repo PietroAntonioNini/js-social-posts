19965abc112ede48fe74a905b15a57436d5ed3fa
PietroAntonioNini <151193470+PietroAntonioNini@users.noreply.github.com>
2024-02-15 17:15:50 +0100

    correzione,creazione degli elementi solo da js

--- js/script.js
+++ js/script.js
@@ -63,8 +63,9 @@ const posts = [
 const postContainer = document.getElementById("container");
 
 //itero attraverso ciascun post nell'array
-posts.slice(1).forEach(post => {
+posts.slice().forEach(post => {
 
+    console.log(post);
     //creo un elemento post
     const postElement = document.createElement("div");
     postElement.classList.add("post");
@@ -105,45 +106,50 @@ posts.slice(1).forEach(post => {
 
     //aggiungo l'elemento di post al contenitore dei post
     postContainer.appendChild(postElement);
-});
 
+    //aggiungo un ascoltatore al tasto mi piace di tutti i post
+    const likeButtonElement = document.querySelectorAll(`#like-button-${post.id}`);
 
+    likeButtonElement.forEach(function(button) {
 
-
-//aggiungo un ascoltatore al tasto mi piace di tutti i post
-const likeButtonElement = document.querySelectorAll("#like-button");
-
-likeButtonElement.forEach(function(button) {
-
-    button.addEventListener('click', function() {
-        
-        //verifico se il pulsante è già stato cliccato
-        const isLiked = button.classList.contains("liked");
-
-        if (isLiked) {
-
-            //rimuovi il "Mi Piace"
-            button.classList.remove("liked");
-
-            //decremento il contatore dei "Mi Piace"
-            const likeCounter = document.querySelector(".js-likes-counter");
-            likeCounter.innerText = parseInt(likeCounter.innerText) - 1;
-
-            //il colore del testo del pulsante torna originale
-            document.querySelector(".like-button__label").style.color = "black";
+        button.addEventListener('click', function(event) {
             
-        } else {
+            console.log("click");
+            //prevengo il comportamento dell'ancor(ricarica la pagina)
+            event.preventDefault();
+            
+            //verifico se il pulsante è già stato cliccato
+            const isLiked = button.classList.contains("liked");
 
-            //aggiungi il "Mi Piace"
-            button.classList.add("liked");
+            if (isLiked) {
 
-            //incremento il contatore dei "Mi Piace"
-            const likeCounter = document.querySelector(".js-likes-counter");
-            likeCounter.innerText = parseInt(likeCounter.innerText) + 1;
+                //rimuovi il "Mi Piace"
+                button.classList.remove("liked");
 
-            //cambio il colore del testo del pulsante
-            document.querySelector(".like-button__label").style.color = "red";
-        }
-        
+                //decremento il contatore dei "Mi Piace"
+                const likeCounter = document.querySelector(".js-likes-counter");
+                likeCounter.innerText = parseInt(likeCounter.innerText) - 1;
+
+                //il colore del testo del pulsante torna originale
+                document.querySelector(".like-button__label").style.color = "black";
+                
+            } else {
+
+                //aggiungi il "Mi Piace"
+                button.classList.add("liked");
+
+                //incremento il contatore dei "Mi Piace"
+                const likeCounter = document.querySelector(".js-likes-counter");
+                likeCounter.innerText = parseInt(likeCounter.innerText) + 1;
+
+                //cambio il colore del testo del pulsante
+                document.querySelector(".like-button__label").style.color = "red";
+            }
+
+        });
     });
 });
+
+
+
+
